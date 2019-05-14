b991f0645e7830e4f6a72c08540df32ff5d1fd75
Dominik <rakdominikrak@gmail.com>
2019-05-14 15:07:28 +0200

    a star repair and brute force using states instead of permutations

--- Komiwojazer/state.py
+++ Komiwojazer/state.py
@@ -1,36 +1,39 @@
 from town import *
+
+
 class State:
-    def __init__(self,towns):
+    def __init__(self, towns, all_towns):
         self.towns = towns
-        self.distance=self.findDistance()
-    def findDistance(self):
-        dis=0
-        for i in range(0, len(self.towns)-1):
-            dis+=self.towns[i].distance(self.towns[i+1])
+        self.distance = self.find_distance()
+        self.heuristic = self.calculate_f(all_towns)
+
+    def find_distance(self):
+        dis = 0
+        for i in range(0, len(self.towns) - 1):
+            dis += self.towns[i].distance(self.towns[i + 1])
         return dis
-    def add_town(self,town):
-        self.towns.append(town)
-        self.distance+=self.towns[-1].distance(self.towns[-2])
-    def delete_last_town(self):
-        self.distance-=self.towns[-1].distance(self.towns[-2])
-        self.towns.pop()
-    def calculate_f(self,towns):
-        temp=999999999999
-        if len(self.towns)==len(towns):
-            return self.distance+self.towns[-1].distance(self.towns[0])*0.1
-        multiplier=len(towns)-len(self.towns)
-        for x in towns:
-            if x not in self.towns:
-                dist=self.towns[-1].distance(x)
-                if dist<temp:
-                    temp=dist
-        return self.distance+temp*multiplier*0.9
+
+    def calculate_f(self, towns):
+        temp = 999999999999
+        this_towns_len = len(self.towns)
+        other_towns_len = len(towns)
+        if this_towns_len == other_towns_len:
+            self.heuristic = self.distance + self.towns[-1].distance(
+                self.towns[0]) * 0.95
+        else:
+            multiplier = other_towns_len - this_towns_len
+            for x in towns:
+                last_town = self.towns[-1]
+                if x not in self.towns:
+                    dist = last_town.distance(x)
+                    if dist < temp:
+                        temp = dist
+            self.heuristic = self.distance + temp * multiplier * 0.95
+        return self.heuristic
 
     def __repr__(self):
-        stateString=""
-        for i in range(0,len(self.towns)):
-            stateString+=str(self.towns[i])+"\n"
-        stateString+="distance = " + str(self.distance)
+        stateString = ""
+        for i in range(0, len(self.towns)):
+            stateString += str(self.towns[i]) + "\n"
+        stateString += "distance = " + str(self.distance)
         return stateString
-
-    
--- Komiwojazer/test.py
+++ Komiwojazer/test.py
@@ -4,22 +4,23 @@ import copy
 import itertools
 import time
 
+
 class Test:
     def __init__(self):
         self.towns = list()
         self.towns = self.load_towns()
-        optimal_start=time.time()
-        self.optimal=self.find_with_shortest()
-        optimal_end=time.time()
-        brute_start=time.time()
-        self.brute=self.brute_force()
-        brute_end=time.time()
-        star_start=time.time()
-        self.star=self.a_star()
-        star_end=time.time()
-        self.brute_time=brute_end-brute_start
-        self.optimal_time=optimal_end-optimal_start
-        self.star_time=star_end-star_start
+        optimal_start = time.time()
+        self.optimal = self.find_with_shortest()
+        optimal_end = time.time()
+        brute_start = time.time()
+        self.brute = self.brute_force()
+        brute_end = time.time()
+        star_start = time.time()
+        self.star = self.a_star()
+        star_end = time.time()
+        self.brute_time = brute_end - brute_start
+        self.optimal_time = optimal_end - optimal_start
+        self.star_time = star_end - star_start
 
     def load_towns(self):
         lista = list()
@@ -27,82 +28,83 @@ class Test:
         with open(filepath) as fp:
             for cnt, line in enumerate(fp):
                 splitted = line.split()
-                lista.append(Town(splitted[0],splitted[1],splitted[2]))
+                lista.append(Town(splitted[0], splitted[1], splitted[2]))
 
         return lista
+
     def find_with_shortest(self):
-        temp_towns=self.towns[:]
-        path=list()
+        temp_towns = self.towns[:]
+        path = list()
         path.append(temp_towns[0])
         temp_towns.remove(path[0])
-        while len(temp_towns)>1:
-            index=0
-            for i in range(1,len(temp_towns)):
-                if path[-1].distance(temp_towns[index])>path[-1].distance(temp_towns[i]):
-                    index=i
+        while len(temp_towns) > 1:
+            index = 0
+            for i in range(1, len(temp_towns)):
+                if path[-1].distance(temp_towns[index]) > path[-1].distance(
+                        temp_towns[i]):
+                    index = i
             path.append(temp_towns.pop(index))
         path.append(temp_towns.pop(0))
         path.append(path[0])
-        return State(path)
+        return State(path, self.towns)
+
     def brute_force(self):
-        #permutations = list(itertools.permutations(self.towns[1:]))
-        permutations = list(self.all_perms(self.towns[1:]))
-        paths=list()
-        for i in range(0,len(permutations)):
-            temp_list=list(permutations[i])
-            temp_list.insert(0,self.towns[0])
-            #temp_list.append(self.towns[0])
-            paths.append(State(temp_list))
-        bestPath=paths[0]
-        for i in range(1,len(paths)):
-            if bestPath.distance>paths[i].distance:
-                bestPath=paths[i]
-        return bestPath
-    def all_perms(self,elements):
-        if len(elements) <=1:
-            yield elements
-        else:
-            for perm in self.all_perms(elements[1:]):
-                for i in range(len(elements)):
-                    yield perm[:i] + elements[0:1] + perm[i:]
-    def a_star(self):
-        states=list()
+        states = list()
         for i in self.towns[1:]:
-            states.append(State([self.towns[0],i]))
+            states.append(State([self.towns[0], i], self.towns))
+        loop_flag = True
+        while loop_flag:
+            temp_states = list()
+            for state in states:
+                temp_states.extend(self.expand_state(state))
+            states = temp_states
+            if len(states[0].towns) == (len(self.towns) + 1):
+                loop_flag = False
+        best_state = states[0]
+        for i in range(1, len(states)):
+            if best_state.distance > states[i].distance:
+                best_state = states[i]
+        return best_state
+
+    def a_star(self):
+        states = list()
+        for i in self.towns[1:]:
+            states.append(State([self.towns[0], i], self.towns))
         #found=False
         while True:
-            state_to_expand=self.find_with_best_heuristic(states)
+            state_to_expand = self.find_with_best_heuristic(states)
+            if len(state_to_expand.towns) == len(self.towns) + 1:
+                return state_to_expand
             states.remove(state_to_expand)
-            expanded_states=self.expand_state(state_to_expand)
-            if len(expanded_states[0].towns)==len(self.towns):
-                return expanded_states[0]
-            states.extend(expanded_states) 
-    def expand_state(self,state):
-        states=list()
-        if len(state.towns)==len(self.towns):
-            states.append(State(state.towns[:]+[state.towns[0]]))
+            expanded_states = self.expand_state(state_to_expand)
+            states.extend(expanded_states)
+
+    def expand_state(self, state):
+        states = list()
+        if len(state.towns) == len(self.towns):
+            states.append(State(state.towns[:] + [state.towns[0]], self.towns))
         else:
             for x in self.towns:
                 if x not in state.towns:
-                    states.append(State(state.towns[:]+[x]))
-        print(states)       
-        
-    
-        return states   
-    def find_with_best_heuristic(self,states):
-        best_state=states[0]
-        best_heuristic=best_state.calculate_f(self.towns)
-        for i in range(1,len(states)):
-            temp_heuristic=states[i].calculate_f(self.towns)
-            if temp_heuristic<best_heuristic:
-                best_heuristic=temp_heuristic
-                best_state=states[i]
+                    states.append(State(state.towns[:] + [x], self.towns))
+        return states
+
+    def find_with_best_heuristic(self, states):
+        best_state = states[0]
+        best_heuristic = best_state.heuristic
+        for state in states:
+            temp_heuristic = state.heuristic
+            if temp_heuristic < best_heuristic:
+                best_heuristic = temp_heuristic
+                best_state = state
         return best_state
-    
+
 
 if __name__ == '__main__':
     test = Test()
-    print("result choosing shortest path each time \n"+str(test.optimal)+ "time: "+str(test.optimal_time))
-    print("result using brute force \n"+str(test.brute) + "time: "+str(test.brute_time))
-    print("result using astar \n"+str(test.star)+ "time: "+str(test.star_time))
-
+    print("result choosing shortest path each time \n" + str(test.optimal) +
+          " |time: " + str(test.optimal_time))
+    print("result using brute force \n" + str(test.brute) + " |time: " +
+          str(test.brute_time))
+    print("result using astar \n" + str(test.star) + " |time: " +
+          str(test.star_time))
